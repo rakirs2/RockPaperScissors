08dac6a37b4cc1885a459514cde013c2e4fb4560
rakirs <srikarmylavarapu@gmail.com>
2022-11-19 08:05:32 -0600

    inintial program

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿// Ask the computer do you want to play a game?
+
+using System.Globalization;
+
+Console.WriteLine("Do you want to play a game of Rock Paper Scissors");
+
+char input = Console.ReadKey().KeyChar;;
+int playerScore = 0;
+int computerScore = 0;
+
+while (Char.ToLower(input, CultureInfo.InvariantCulture) == 'y' )
+{
+    input = Console.ReadKey().KeyChar;
+}
+
+HashSet<char> validKeys = new HashSet<char>()
+{
+    'r', 'p', 's'
+};
+
+
+
+
+
+
+// This exists in python but you do not have to do it this way
+enum Values{
+    Rock, Paper, Scissors
+}
+
+
+
+
+
